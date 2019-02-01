3af3e4654129e0b8ee095da302b05cb02a3da78d
paflov <philipp.franck@dainst.de>
2019-02-01 16:53:06 +0100

    added method stubs for plugin dummy

--- cilantro.inc.php
+++ cilantro.inc.php
@@ -3,5 +3,24 @@ import('lib.pkp.classes.plugins.GenericPlugin');
 
 class cilantro extends GenericPlugin {
 
+	/**
+	 * Get the display name for this plugin.
+	 *
+	 * @return string
+	 */
+	function getDisplayName()
+	{
+		// TODO: Implement getDisplayName() method.
+	}
+
+	/**
+	 * Get a description of this plugin.
+	 *
+	 * @return string
+	 */
+	function getDescription()
+	{
+		// TODO: Implement getDescription() method.
+	}
 }
 ?>
